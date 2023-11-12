d54ff818eae9b3ac6dc2e048acfb0390e71db6e6
MandeepKS <129056226+MandeepKS@users.noreply.github.com>
2023-11-12 13:08:36 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ import { jacketscollectionHomePage } from "./home.js";
 import { jacketPage } from "./postjacket.js";
 import { jacketscollectionPage } from "./postjackets.js";
 
-
+if(location.pathname === " ")
+{
+  jacketscollectionHomePage();
+}
 if(location.pathname === "/index")
 {
   jacketscollectionHomePage();
