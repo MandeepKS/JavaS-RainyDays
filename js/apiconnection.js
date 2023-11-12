0c7effbd382c4a3470103330a789f79b03839c9e
MandeepKS <129056226+MandeepKS@users.noreply.github.com>
2023-11-12 13:59:58 +0100

    Update apiconnection.js
    error handling

--- js/apiconnection.js
+++ js/apiconnection.js
@@ -1,5 +1,5 @@
  const url = "https://api.noroff.dev";
- const endpoint = "/api/v1/rainy-days/";
+ const endpoint = "/api/v1/rainy-day/";
  const apiUrl = url + endpoint;
 
  export {url, endpoint, apiUrl}
